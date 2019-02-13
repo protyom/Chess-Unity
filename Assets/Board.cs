3a166075896f09c7c91a59f4afb0a04125d9e196
protyom <protyom@gmail.com>
2019-02-13 23:10:16 +0300

    Minimax, check, mate, enhanced models

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -28,9 +28,10 @@ namespace Assets {
         private bool isSelected;
         private Team makesMove;
 
-        private const int MINIMAX_DEPTH = 3;
+        private const int MINIMAX_DEPTH = 2;
         private bool isPlaying = false;
         private bool isFinished = false;
+        private bool isInstruction = false;
         private string winner;
 
         private bool isBotPlaying = false;
@@ -216,7 +217,7 @@ namespace Assets {
                 changeTurn();
             }
         }
-        public void checkWon() {
+        /*public void checkWon() {
             Move[] m = moves.ToArray();
             if (m.Length > 0) {
                 if (m[m.Length - 1].Beaten != null) {
@@ -232,6 +233,19 @@ namespace Assets {
                     }
                 }
             }
+        }*/
+
+        public void checkWon() {
+            if (isMate(Team.WHITE)) {
+                winner = "Black";
+                isFinished = true;
+                isPlaying = false;
+            }
+            if (isMate(Team.BLACK)) {
+                winner = "White";
+                isFinished = true;
+                isPlaying = false;
+            }
         }
 
         public void placeCell(GameObject cell, Vector2Int pos) {
@@ -242,8 +256,9 @@ namespace Assets {
             wpos.x -= 0.45f;
             cell.transform.position = wpos;
         }
-        
-        public List<Vector2Int> determineMoves(Vector2Int movingPos) {
+
+
+        /*public List<Vector2Int> determineMoves(Vector2Int movingPos) {
             List<Vector2Int> correctMoves = new List<Vector2Int>();
             GameObject moving = board[movingPos.x, movingPos.y];
             Figure figMoving = moving.GetComponent<Figure>();
@@ -259,6 +274,137 @@ namespace Assets {
                         if (figtemp.team == figMoving.team) {
                             continue;
                         }
+
+                    }
+                    moveFigure(movingPos, moves[i] + movingPos);
+                    if (isCheck(figMoving.team)) {
+                        undoMove();
+                        continue;
+                    }
+                    undoMove();
+                    correctMoves.Add(moves[i] + movingPos);
+                }
+
+
+            }
+
+
+
+            if (figMoving.VectorMoves != null) {
+                Vector2Int[] vMoves = figMoving.VectorMoves;
+                for (int i = 0; i < vMoves.Length; i++) {
+                    Vector2Int step = vMoves[i];
+                    if (step.x != 0) {
+                        step.x /= Math.Abs(step.x);
+                    }
+                    if (step.y != 0) {
+                        step.y /= Math.Abs(step.y);
+                    }
+                    int num = 1;
+                    while (step * num != vMoves[i]) {
+                        if ((movingPos + step * num).x < 0 || (movingPos + step * num).x > 7 ||
+                            (movingPos + step * num).y < 0 || (movingPos + step * num).y > 7) {
+                            break;
+                        }
+                        if (board[(movingPos + step * num).x, (movingPos + step * num).y] != null) {
+                            Figure figtemp = board[(movingPos + step * num).x, (movingPos + step * num).y].GetComponent<Figure>();
+                            if (figtemp.team == figMoving.team || figMoving.Type == TypeFigure.Pawn) {
+                                break;
+                            }
+                            moveFigure(movingPos, movingPos + step * num);
+                            if (isCheck(figMoving.team)) {
+                                undoMove();
+                                continue;
+                            }
+                            undoMove();
+                            correctMoves.Add((movingPos + step * num));
+                            break;
+                        }
+                        moveFigure(movingPos, movingPos + step * num);
+                        if (isCheck(figMoving.team)) {
+                            undoMove();
+                            break;
+                        }
+                        undoMove();
+                        correctMoves.Add((movingPos + step * num));
+                        num++;
+                    }
+                }
+            }
+            if (!figMoving.WasMoved && figMoving.Type == TypeFigure.Pawn) {
+                Vector2Int additional = new Vector2Int(0, 2);
+                if (figMoving.team == Team.BLACK) {
+                    additional *= -1;
+                }
+                additional += movingPos;
+                if (board[additional.x, additional.y] == null) {
+                    moveFigure(movingPos, additional);
+                    if (!isCheck(figMoving.team)) {
+                        correctMoves.Add(additional);
+                    }
+                    undoMove();
+
+
+
+                }
+            }
+
+            if (figMoving.AttackMoves != null) {
+                Vector2Int[] attackMoves = figMoving.AttackMoves;
+
+                for (int i = 0; i < attackMoves.Length; i++) {
+                    if ((attackMoves[i] + movingPos).x < 0 || (attackMoves[i] + movingPos).x > 7 || (attackMoves[i] + movingPos).y < 0 || (attackMoves[i] + movingPos).y > 7) {
+                        continue;
+                    }
+                    if (board[(attackMoves[i] + movingPos).x, (attackMoves[i] + movingPos).y] != null) {
+                        Figure figtemp = board[(attackMoves[i] + movingPos).x, (attackMoves[i] + movingPos).y].GetComponent<Figure>();
+                        if (figtemp.team != figMoving.team) {
+                            moveFigure(movingPos, attackMoves[i] + movingPos);
+                            if (isCheck(figMoving.team)) {
+                                undoMove();
+                                continue;
+                            }
+                            undoMove();
+                            correctMoves.Add(attackMoves[i] + movingPos);
+                        }
+                    }
+
+                }
+            }
+            return correctMoves;
+        }*/
+
+        public List<Vector2Int> determineMoves(Vector2Int movingPos) {
+            List<Vector2Int> correctNoCheckMoves = determineMovesNoCheck(movingPos);
+            Figure figMoving = board[movingPos.x, movingPos.y].GetComponent<Figure>();
+            List<Vector2Int> correctMoves = new List<Vector2Int>();
+            foreach (Vector2Int move in correctNoCheckMoves) {
+                moveFigure(movingPos, move);
+                if (!isCheck(figMoving.team)) {
+                    correctMoves.Add(move);
+                }
+                undoMove();
+            }
+            return correctMoves;
+        }
+
+        private List<Vector2Int> determineMovesNoCheck(Vector2Int movingPos) {
+            List<Vector2Int> correctMoves = new List<Vector2Int>();
+            GameObject moving = board[movingPos.x, movingPos.y];
+            Figure figMoving = moving.GetComponent<Figure>();
+            Vector2Int[] moves = figMoving.Moves;
+            if (moves != null) {
+
+                for (int i = 0; i < moves.Length; i++) {
+                    if ((moves[i] + movingPos).x < 0 || (moves[i] + movingPos).x > 7 || (moves[i] + movingPos).y < 0 || (moves[i] + movingPos).y > 7) {
+                        continue;
+                    }
+                    if (board[(moves[i] + movingPos).x, (moves[i] + movingPos).y] != null) {
+                        Figure figtemp = board[(moves[i] + movingPos).x, (moves[i] + movingPos).y].GetComponent<Figure>();
+                        if (figtemp.team == figMoving.team) {
+                            continue;
+                        }
+
                     }
                     correctMoves.Add(moves[i] + movingPos);
                 }
@@ -305,6 +451,8 @@ namespace Assets {
                 additional += movingPos;
                 if (board[additional.x, additional.y] == null) {
                     correctMoves.Add(additional);
+
+
                 }
             }
 
@@ -346,6 +494,37 @@ namespace Assets {
             }
         }
 
+
+        public bool isCheck(Team t){
+            for(int i=0;i<8;i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (board[i, j] != null) {
+                        Figure fig = board[i, j].GetComponent<Figure>();
+                        if (fig.team != t) {
+                            List<Vector2Int> moves = determineMovesNoCheck(new Vector2Int(i, j));
+                            foreach(Vector2Int move in moves) {
+                                if (board[move.x, move.y]!=null) {
+                                    Figure fig2 = board[move.x, move.y].GetComponent<Figure>();
+                                    if (fig2.Type == TypeFigure.King) {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+
+
+        public bool isMate(Team forTeam) {
+            List<Move> all = allMoves(forTeam);
+            Move[] m = all.ToArray();
+            return (m.Length == 0);
+        }
+
         public int evaluateBoard() {
             int res = 0;
             foreach(GameObject cell in board) {
@@ -362,13 +541,13 @@ namespace Assets {
             }
             return res;
         }
-        public List<Move> allMoves() {
+        public List<Move> allMoves(Team forTeam) {
             List<Move> all = new List<Move>();
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
                     if (board[i,j] != null) {
                         Figure f = board[i, j].GetComponent<Figure>();
-                        if (f.team == makesMove) {
+                        if (f.team == forTeam) {
                             Vector2Int gamePos = new Vector2Int(i,j);
                             List<Vector2Int> figMoves = determineMoves(gamePos);
                             foreach (Vector2Int move in figMoves) {
@@ -382,34 +561,51 @@ namespace Assets {
             }
             return all;
         }
-        public int minimax(int depth,bool isMaximizing) {
+        public int minimax(int depth,int alpha, int beta, bool isMaximizing) {
             if (depth == 0) {
                 return -evaluateBoard();
             }
             if (isMaximizing) {
-                List<Move> all = allMoves();
+                List<Move> all = allMoves(Team.BLACK);
                 Move[] m = all.ToArray();
                 int max = int.MinValue;
                 for (int i = 0; i < m.Length; i++) {
                     moveFigure(m[i].OldPoint, m[i].EndPoint);
-                    max = Math.Max(max, minimax(depth-1, !isMaximizing));
+                    if (isMate(Team.WHITE)) {
+                        undoMove();
+                        return Convert.ToInt32(TypeFigure.King);
+                    }
+                    max = Math.Max(max, minimax(depth-1,alpha,beta, !isMaximizing));
                     undoMove();
+                    alpha = Math.Max(alpha, max);
+                    if (alpha >= beta){
+                        return max;
+                    }
                 }
                 return max;
             } else {
-                List<Move> all = allMoves();
+                List<Move> all = allMoves(Team.WHITE);
                 Move[] m = all.ToArray();
                 int min = int.MaxValue;
                 for (int i = 0; i < m.Length; i++) {
                     moveFigure(m[i].OldPoint, m[i].EndPoint);
-                    min = Math.Max(min, minimax(depth - 1, !isMaximizing));
+                    if (isMate(Team.WHITE)) {
+                        undoMove();
+                        return -Convert.ToInt32(TypeFigure.King);
+                    }
+                    min = Math.Min(min, minimax(depth - 1, alpha, beta, !isMaximizing));
                     undoMove();
+                    beta = Math.Min(beta, min);
+                    if (alpha >= beta)
+                    {
+                        return min;
+                    }
                 }
                 return min;
             }
         }
         public Move determineBestBotMove() {
-            List<Move> all = allMoves();
+            List<Move> all = allMoves(Team.BLACK);
             Move[] m = all.ToArray();
             //System.Random r = new System.Random();
             //return m[r.Next(m.Length)];         /** Random move**/
@@ -418,7 +614,7 @@ namespace Assets {
             
             for(int i = 0; i < m.Length; i++) {
                 moveFigure(m[i].OldPoint, m[i].EndPoint);
-                int eval = minimax(MINIMAX_DEPTH-1,true);
+                int eval = minimax(MINIMAX_DEPTH-1,int.MinValue,int.MaxValue,false);
                 if (eval > max) {
                     max = eval;
                     maxi = i;
@@ -430,21 +626,29 @@ namespace Assets {
         }
 
         public void OnGUI() {
-            if (!isPlaying && !isFinished) {
+            if (!isPlaying && !isFinished && !isInstruction) {
                 if (GUI.Button(new Rect(110, 110, 100, 40), "Play 1 vs 1")) {
                     startGame();
                 }
                 if (GUI.Button(new Rect(220, 110, 100, 40), "Play with Bot")) {
                     startBotGame();
                 }
-                if (GUI.Button(new Rect(330, 110, 100, 40), "Exit")) {
+                if (GUI.Button(new Rect(330, 110, 100, 40), "How to play")) {
+                    isInstruction = true; ;
+                }
+                if (GUI.Button(new Rect(440, 110, 100, 40), "Exit")) {
                     Application.Quit();
                 }
             }
-
+            if (isInstruction) {
+                GUI.Box(new Rect(50, 50, 300, 300), "Press left mouse button to select cell,\n right to deselect,\n esc to go to main menu");
+                if (GUI.Button(new Rect(150, 150, 100, 40), "Ok")) {
+                    isInstruction = false;
+                }
+            }
             if (isFinished) {
-                GUI.Box(new Rect(10, 10, 300, 300), winner + " is the winner!");
-                if (GUI.Button(new Rect(50, 50, 100, 40), "Ok")) {
+                GUI.Box(new Rect(50, 50, 300, 300), winner + " is the winner!");
+                if (GUI.Button(new Rect(150, 150, 100, 40), "Ok")) {
                     isFinished = false;
                     isPlaying = false;
                     finishGame();
@@ -617,6 +821,7 @@ namespace Assets {
                                 }
                                 moveCells.Clear();
                                 moveFigure(selectedFigure, cell);
+                                checkWon();
                             }
                         }
                     }
@@ -643,7 +848,7 @@ namespace Assets {
 
             if (isPlaying && !isBotPlaying) {
                 handleUserUpdate();
-                checkWon();
+
                 if (Input.GetKeyDown(KeyCode.Backspace) && !isSelected && !isBotPlaying) {
                     undoMove();
                 }
@@ -660,9 +865,10 @@ namespace Assets {
                 } else {
                     Move m = determineBestBotMove();
                     moveFigure(m.OldPoint,m.EndPoint);
+                    checkWon();
                     //changeTurn();
                 }
-                checkWon();
+
             }
         }
     }
